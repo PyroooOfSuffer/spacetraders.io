69c7cf2c0cc8dc321db92bee19c76fe0267a3be7
PyroooOfSuffer <benjamin.trautweiler.8105@gmail.com>
2023-09-10 22:40:08 +0200

    pain

--- main.py
+++ main.py
@@ -6,16 +6,14 @@ import spacetrader_api as api
 venv - not on GitHub -   - not code -
 > virtual environment for PyCharm
 
+xold - folder - 
+>> contains usable files that i dont need for the current interation of the program
+
 .gitignore  - not code -
 > which files to not update on GitHub
 
 auth_token.txt - not on GitHub -  - not code -
 > the token as a single string
-
-bot.py
-> automate some process (for starters update all ships +  my_agent + current system/waypoints?)
->> data transforming for db here or in database.py?? or create updater.py?
->> create a universal function to read out date of any response? or add every response from hand?
 
 console.py
 > allow control of the program over console (currently able to use spacetrader_api func)
@@ -32,13 +30,10 @@ requirements.txt - not code -
 simple_automation.py
 > some easy automation, currently contains:
 >> generate a token + write into auth_token.txt
->> flatten dicts
->> create column_type for the create_table in database.py
->> create column_data for the add_data in database.py
 
 spacetrader_api.py - no change needed -
 > all requests to API as functions
 """
 
 if __name__ == "__main__":
-    print(prototype_navigation.denest_dict(prototype_data.ship_list_data[0]))
+    print("send HELP")
